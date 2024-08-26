e3417ffd71a96807049730b08ea5c37946f3accf
hamzagbouri <167026130+hamzagbouri@users.noreply.github.com>
2024-08-26 23:29:03 +0200

    Update axiosInstance.js

--- src/pages/ApiCalls/axiosInstance.js
+++ src/pages/ApiCalls/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://backend-ibtissam.infinityfreeapp.com/api',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
