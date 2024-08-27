4bff6333d63c58aabebd6fc3295a160b97a76dc9
hamzagbouri <hamzagbouri2004@gmail.com>
2024-08-27 23:32:48 +0200

    uodate api

--- src/pages/ApiCalls/axiosInstance.js
+++ src/pages/ApiCalls/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://storemanagement-hce0c0cqddagapcw.eastus-01.azurewebsites.net/api',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
--- src/pages/ApiCalls/axiosWithToken.js
+++ src/pages/ApiCalls/axiosWithToken.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosWithToken = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://storemanagement-hce0c0cqddagapcw.eastus-01.azurewebsites.net/api/',
     headers: {
         'Content-Type': 'application/json',
     },
