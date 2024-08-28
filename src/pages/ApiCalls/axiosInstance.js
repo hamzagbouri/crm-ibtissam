f69735c8f6dcc655f0ef14c73b3484c5e5d82f10
hamzagbouri <167026130+hamzagbouri@users.noreply.github.com>
2024-08-28 20:05:18 +0200

    Update axiosInstance.js

--- src/pages/ApiCalls/axiosInstance.js
+++ src/pages/ApiCalls/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://store-management-backend-ckhzcpd6cyczfped.eastus-01.azurewebsites.net',
+    baseURL: 'https://store-management-backend-ckhzcpd6cyczfped.eastus-01.azurewebsites.net/api',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
